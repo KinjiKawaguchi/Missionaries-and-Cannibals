1c80c856792e46db61a2678a7840e90b368968f1
KinjiKawaguchi <kawakin0310@icloud.com>
2022-11-16 15:44:14 +0900

    プロジェクト ファイルを追加します。

---
+++ Missionaries and Cannibals/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Missionaries_and_Cannibals
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
